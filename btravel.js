cbf30aa6183cff4eb701a67989d875c055de6450
Dmitriy Sukhov <dstujev@gmail.com>
2017-09-11 19:27:00 +0300

    it works properly

--- btravel.js
+++ btravel.js
@@ -1,30 +1,16 @@
-'use strict'
-  var sum = new Set();
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+var sum = new Set();
 
-
-function chooseBestSum(/*max distance*/ t, /*towns to visit*/ k, ls) {
-
-  var max = 0;
-
-  function counting(pass, arr, count) {
-    if(pass > 0) {
-      for (let i = 0; i < arr.length-pass; i++) {
-        counting(pass-1, arr.slice(i+1), count+arr[i]);
-      };
+function counting(level, arr, count) {
+  for (let i = 0; i < arr.length - level; i++) {
+    if (level > 0) {
+      counter(level-1, arr.slice(i+1), count+arr[i]);
     } else {
-      console.log(arr[0]);
-      sum.add(count+arr[0]);
-    };
-  };
+      sum.add(count+arr[i]);
+    }
+  }
+}
 
-  counting(k-1, ls, 0);
-  sum.forEach(function(a) {
-      if(a>max && a <= t) max = a;
-  });
-
-  return max > 0 ? max : null;
-};
-
-var ts = [50, 55, 57, 58, 60];
-console.log(chooseBestSum(174, 3, ts));
+counter(2, array, 0);
 console.log(sum);
+
